0cd9fbbf0305ff58f1dc992d5ae6e624d76479b1
Evgeny <greatly01@ya.ru>
2020-10-14 10:01:49 +0500

    Initial Commit

---
+++ HW 2.1 - Task 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2.1 - Task 2
+//
+//  Created by User on 14.10.2020.
+//  Copyright © 2020 Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
